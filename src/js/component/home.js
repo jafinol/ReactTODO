426be38d23e92bfff9a7c042fe8e53cbad2378e7
jafinol <jafinol@gmail.com>
2021-02-27 03:34:37 +0000

    some commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,32 +3,32 @@ import React, { useState } from "react";
 export function Home() {
 	const [todoList, setTodoList] = useState([]);
 	const [todo, setTodo] = useState("");
-	const [sizeArr, setSizeArr] = useState(0);
+	const [trash, setTrash] = useState(false);
+	const [trashList, setTrashList] = useState([]);
 
 	//create your first component
 	const updateTodo = e => {
 		setTodo(e.target.value);
-		setSizeArr(todoList.length);
 	};
 
 	const handleAdd = () => {
 		setTodoList([...todoList, todo]);
-
+		setTrashList([...trashList, false]);
 		setTodo("");
-		setSizeArr(todoList.length);
 	};
 
-	const deleteElem = index => {
-		todoList.splice(index, 1);
-		handleAdd();
-		setSizeArr(todoList.length);
-	};
-
-	// setSizeArr(todoList.length);
+	function deleteElem(arr, itemdel) {
+		const result = arr.filter((todoList, index3) => {
+			return index3 != itemdel;
+		});
+		setTodoList(result);
+	}
 
 	return (
-		<div className="container ">
-			<div className=" text-center container mt-5 todoListMain2">
+		<div className="container " style={{ with: "30%", margin: "auto" }}>
+			<div
+				className=" text-center container mt-5 todoListMain2"
+				style={{ with: "30%", margin: "auto" }}>
 				<div>
 					<h2>
 						<em> todos</em>
@@ -47,15 +47,29 @@ export function Home() {
 			</div>
 
 			{todoList.map((item, index) => (
-				<p
+				<div
 					key={index}
-					className="todoListMain container"
-					onClick={() => deleteElem(index)}>
-					{item}
-				</p>
+					onMouseEnter={() => setTrash(!trash)}
+					onMouseLeave={() => setTrash(!trash)}
+					//onMouseEnter={() => (trashList[index] = !trash)}
+					//onMouseLeave={() => (trashList[index] = !trash)}
+					className="todoListMain container row"
+					style={{ with: "30%", margin: "auto" }}
+					onClick={() => deleteElem(todoList, index)}>
+					<div className="d-flex justify-content-start col">
+						{item}
+					</div>
+					<div className="d-flex justify-content-end col">
+						{!trash === false ? (
+							<i className="fas fa-trash-alt"></i>
+						) : (
+							""
+						)}
+					</div>
+				</div>
 			))}
 
-			<p className="todoListFoo container">{sizeArr} item left</p>
+			<p className="todoListFoo container">{todoList.length} item left</p>
 		</div>
 	);
 }
